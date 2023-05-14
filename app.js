4baa53b7ecc90a01b540c3bc7a704a6dc78c5345
Fernando Orduy <53240305+ferchorduy@users.noreply.github.com>
2023-05-14 04:47:33 -0400

    Layout essentially finished, now must add functionality to user input

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 const grid = document.createElement('div')
-grid.classList.add('grid');
-grid.setAttribute('style', 'height: 640px; width: 640px; border: solid 1px black; background-color: purple;')
 const containerMain = document.getElementById('container-main');
+
+grid.classList.add('grid');
 containerMain.appendChild(grid);
+
+for (let i = 0; i < 256; i++) {
+
+  const gridPiece = document.createElement('div');
+  gridPiece.classList.add('grid-piece');
+  grid.appendChild(gridPiece);
+
+}
+
+// const gridPieces = document.getElementsByClassName('grid-piece');
+// Array.from(gridPieces).forEach( (gridPiece) => {
+//   gridPiece.addEventListener('mouseenter', () => {
+//     gridPiece.classList.add('colored');
+//   });
+// });
+
+const squareInput = document.createElement('input');
+const colorChoice = document.createElement('div');
+const rainbowColor = document.createElement('button');
+const greyShadesColor = document.createElement('button');
+squareInput.classList.add('input-x');
+colorChoice.classList.add('color-choice');
+rainbowColor.classList.add('rainbow-color');
+greyShadesColor.classList.add('grey-shades-color');
+
+squareInput.placeholder = "x&y";
+rainbowColor.textContent = "rainbow";
+greyShadesColor.textContent = "greyshades"
+
+containerMain.insertBefore(squareInput, grid);
+containerMain.appendChild(colorChoice);
+colorChoice.appendChild(rainbowColor);
+colorChoice.appendChild(greyShadesColor);
